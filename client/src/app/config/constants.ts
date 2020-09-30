6308b3a943354b9377368ef3722c0a97b59a8e22
Elena Bychkova <elenadev777@gmail.com>
2020-09-30 10:40:19 +0300

    test

--- client/src/app/config/constants.ts
+++ client/src/app/config/constants.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-// @ts-ignore
+
 export class Constants {
   public readonly URL: string = 'https://simple-organize-app-2-level.herokuapp.com';
 }
